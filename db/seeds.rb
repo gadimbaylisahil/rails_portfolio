66af1e07eaaef1a7aff42393d42226e448120c4a
Sahil <sahil.gadimbay@gmail.com>
2017-09-10 00:03:09 +0200

    portfolio controller, routes and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "Blog post #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel ornare sem, non ultricies metus. Proin sed ipsum at lacus imperdiet molestie vitae porttitor erat. Aenean sit amet sagittis felis. 
+					 Suspendisse sit amet egestas tellus, vel molestie leo. Mauris a mauris turpis. Vivamus vestibulum ac enim at iaculis. Etiam finibus aliquet felis, et tristique nunc fermentum volutpat. In tristique leo ex, sed rhoncus ante tempus ut. 
+					 Proin sit amet eleifend dui, sed maximus erat. Pellentesque mattis mauris faucibus rutrum cursus. Maecenas vel leo nibh. Cras sodales mauris id fringilla ultrices. Quisque lobortis orci et luctus consectetur. Aliquam erat volutpat."
+		)
+end
+
+puts "10 blogs have been created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percentage: 15
+		)
+end
+
+puts "5 skills have been created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "Service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel ornare sem, non ultricies metus. Proin sed ipsum at lacus imperdiet molestie vitae porttitor erat. 
+		Aenean sit amet sagittis felis. Suspendisse sit amet egestas tellus, vel molestie leo. Mauris a mauris turpis. 
+		Vivamus vestibulum ac enim at iaculis. Etiam finibus aliquet felis, et tristique nunc fermentum volutpat. In tristique leo ex, sed rhoncus ante tempus ut. Proin sit amet eleifend dui, sed maximus erat. Pellentesque mattis mauris faucibus rutrum cursus. Maecenas vel leo nibh. Cras sodales mauris id fringilla ultrices. Quisque lobortis orci et luctus consectetur. Aliquam erat volutpat.",
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200"
+		)
+end
