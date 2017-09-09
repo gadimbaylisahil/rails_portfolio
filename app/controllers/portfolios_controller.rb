3949f7a6f9258819c6abd69d474520d4d623f8da
Sahil <sahil.gadimbay@gmail.com>
2017-09-10 00:34:58 +0200

    basic crud actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,31 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def create
+		@portfolio_item = Portfolio.new(portfolio_item_params)
+		respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	private
+
+	def portfolio_item_params
+		params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+	end
 end
