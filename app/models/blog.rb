51b2ec5495071dd66284f50d5757a73bcff23cd0
Sahil <sahil.gadimbay@gmail.com>
2017-09-10 02:31:13 +0200

    add status enum to scope drafts and published blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
+  # Database sope by using enums
+  # Blog.draft/published will be accessible in the model
+  enum status: { draft: 0, published: 1}
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
