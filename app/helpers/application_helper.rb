46bcb37f64dc0c9232f3bc88eb8199e9a925e6ab
Sahil <sahil.gadimbay@gmail.com>
2017-10-04 00:05:08 +0200

    added source helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       (link_to 'Register', new_user_registration_path)
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      content_tag(:p, "Thanks for visiting me from #{session[:source]}", class: "source-greeting")
+    end
+  end
 end
