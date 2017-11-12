5d12e0967873bb44aedffbe41ad3a25a0cdbe584
Sahil <sahil.gadimbay@gmail.com>
2017-11-12 12:13:40 +0100

    add scope by_position on Portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,10 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
   after_initialize :set_defaults
+  
+  def self.by_position
+    order("position ASC")
+  end
 
   def set_defaults
     self.main_image  ||= Placeholder.image_generator(400, 600)
