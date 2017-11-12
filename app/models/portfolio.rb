a91573035336fb0fabfab38e9a679d9dd6bf4296
Sahil <sahil.gadimbay@gmail.com>
2017-11-12 21:37:30 +0100

    implemented file uploading via carrierwave

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,10 @@ class Portfolio < ApplicationRecord
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image,  PortfolioUploader
+
   after_initialize :set_defaults
   
   def self.by_position
